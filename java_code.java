d4a731f880395766bb755d1363cda776b8d2f164
DevVSantosh <72922150+DevVSantosh@users.noreply.github.com>
2021-02-18 12:42:37 +0100

    Add files via upload
    Java code structure.

---
+++ java_code.java
@@ -0,0 +1,265 @@
+----------------------------------------------------------------------------------------------------------------------------------------------------------
+	-- Java code units - 
+----------------------------------------------------------------------------------------------------------------------------------------------------------
+	-- CreateCustomerDetails
+----------------------------------------------------------------------------------------------------------------------------------------------------------
+	
+/*
+ *
+ * Specify required comments
+ *
+ */
+package com.ib.createentitycls;
+
+import java.io.*;
+import java.util.*;
+
+/*
+ *
+ * Specify required comments
+ *
+ */
+public class CreateCustomerDetails{
+	public static final String CLASS_NAME = "CreateCustomerDetails: ";
+	public static void main(String args[]){
+		String METHOD_NAME = "main(): ";
+		System.out.println(CLASS_NAME + METHOD_NAME + "Begining of the Main Method...");
+		System.out.println(CLASS_NAME + METHOD_NAME + "End of the Main Method...");
+	}
+}//End of class
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------
+	-- CreateCustomerAccounts
+----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+/*
+ *
+ * Specify required comments
+ *
+ */
+package com.ib.createentitycls;
+
+import java.io.*;
+import java.util.*;
+
+/*
+ *
+ * Specify required comments
+ *
+ */
+public class CreateCustomerAccounts{
+	public static final String CLASS_NAME = "CreateCustomerAccounts: ";
+	public static void main(String args[]){
+		String METHOD_NAME = "main(): ";
+		System.out.println(CLASS_NAME + METHOD_NAME + "Begining of the Main Method...");
+		System.out.println(CLASS_NAME + METHOD_NAME + "End of the Main Method...");
+	}
+}//End of class
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------
+	-- CreateHOsetup
+----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+/*
+ *
+ * Specify required comments
+ *
+ */
+package com.ib.createentitycls;
+
+import java.io.*;
+import java.util.*;
+
+/*
+ *
+ * Specify required comments
+ *
+ */
+public class CreateHOsetup{
+	public static final String CLASS_NAME = "CreateHOsetup: ";
+	public static void main(String args[]){
+		String METHOD_NAME = "main(): ";
+		System.out.println(CLASS_NAME + METHOD_NAME + "Begining of the Main Method...");
+		System.out.println(CLASS_NAME + METHOD_NAME + "End of the Main Method...");
+	}
+}//End of class
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------
+	-- CreateBranchsetup
+----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+/*
+ *
+ * Specify required comments
+ *
+ */
+package com.ib.createentitycls;
+
+import java.io.*;
+import java.util.*;
+
+/*
+ *
+ * Specify required comments
+ *
+ */
+public class CreateBranchsetup{
+	public static final String CLASS_NAME = "CreateBranchsetup: ";
+	public static void main(String args[]){
+		String METHOD_NAME = "main(): ";
+		System.out.println(CLASS_NAME + METHOD_NAME + "Begining of the Main Method...");
+		System.out.println(CLASS_NAME + METHOD_NAME + "End of the Main Method...");
+	}
+}//End of class
+
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------
+	-- CreateProductsetup
+----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+/*
+ *
+ * Specify required comments
+ *
+ */
+package com.ib.createentitycls;
+
+import java.io.*;
+import java.util.*;
+
+/*
+ *
+ * Specify required comments
+ *
+ */
+public class CreateProductsetup{
+	public static final String CLASS_NAME = "CreateProductsetup: ";
+	public static void main(String args[]){
+		String METHOD_NAME = "main(): ";
+		System.out.println(CLASS_NAME + METHOD_NAME + "Begining of the Main Method...");
+		System.out.println(CLASS_NAME + METHOD_NAME + "End of the Main Method...");
+	}
+}//End of class
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------
+	-- CreateProdprefsetup. 
+----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+/*
+ *
+ * Specify required comments
+ *
+ */
+package com.ib.createentitycls;
+
+import java.io.*;
+import java.util.*;
+
+/*
+ *
+ * Specify required comments
+ *
+ */
+public class CreateProdprefsetup{
+	public static final String CLASS_NAME = "CreateProdprefsetup: ";
+	public static void main(String args[]){
+		String METHOD_NAME = "main(): ";
+		System.out.println(CLASS_NAME + METHOD_NAME + "Begining of the Main Method...");
+		System.out.println(CLASS_NAME + METHOD_NAME + "End of the Main Method...");
+	}
+}//End of class
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------
+	-- CreateCustPortFolio
+----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+/*
+ * This method is used to process the sample digital codes
+ * @ param String       strPf_custid
+ * @ param String       strPf_asset_code
+ * @ param String       strPf_asset_type
+ * @ param Int            iPf_asset_qty
+ * @ param String       strPf_asset_ccy
+ * @ param Int           iPf_asset_value
+ * @ param String       strPf_last_book_date
+ * @ param String       strPf_last_setl_date
+ * @ param String       strPf_setl_refno
+ * @ exception IOException
+ * @ exception Exception
+ * @ return void
+ */
+public void CreateCustPortFolio(String       strPf_custid,String       strPf_asset_code,String       strPf_asset_type,Int            iPf_asset_qty,String       strPf_asset_ccy,Int           iPf_asset_value,String       strPf_last_book_date,String       strPf_last_setl_date,String       strPf_setl_refno)throws IOException,Exception{
+	String METHOD_NAME = "CreateCustPortFolio(): ";
+	System.out.println(CLASS_NAME + METHOD_NAME + "Begining of method...");
+	//Enter your business logic here 
+	System.out.println(CLASS_NAME + METHOD_NAME + "End of method...");
+}//End of CreateCustPortFolio method...
+
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------
+	-- CreateCustTradeInstr
+----------------------------------------------------------------------------------------------------------------------------------------------------------	
+/*
+ * This method is used to process the sample digital codes
+ * @ param String 			strTi_custid
+ * @ param String 			strTi_ruleid
+ * @ param String 			strTi_rule_type
+ * @ param String 			strTi_asset_code
+ * @ param String 			strTi_asset_type
+ * @ param String 			strTi_buysell_ind
+ * @ param String 			strTi_mktorlmt
+ * @ param Int 			iTi_asset_qty
+ * @ param String 			strTi_asset_ccy
+ * @ param Int 			iTi_asset_value
+ * @ param String 			strTi_sip_indicator
+ * @ param String 			strTi_trade_st_date
+ * @ param String 			strTi_trade_end_dat
+ * @ param String 			strTi_trade_remrks
+ * @ exception IOException
+ * @ exception Exception
+ * @ return void
+ */
+public void CreateCustTradeInstr(String 			strTi_custid,String 			strTi_ruleid,String 			strTi_rule_type,String 			strTi_asset_code,String 			strTi_asset_type,String 			strTi_buysell_ind,String 			strTi_mktorlmt,Int 			iTi_asset_qty,String 			strTi_asset_ccy,Int 			iTi_asset_value,String 			strTi_sip_indicator,String 			strTi_trade_st_date,String 			strTi_trade_end_dat,String 			strTi_trade_remrks)throws IOException,Exception{
+	String METHOD_NAME = "CreateCustPortFolio(): ";
+	System.out.println(CLASS_NAME + METHOD_NAME + "Begining of method...");
+	//Enter your business logic here 
+	System.out.println(CLASS_NAME + METHOD_NAME + "End of method...");
+}//End of CreateCustPortFolio method...
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+/*
+ * Specify required comment
+ */
+package com.ib.createentitycls;
+
+import java.io.*;
+import java.util.*;
+/*
+ * Specify required comment
+ */
+public class CreateTradeRulesetup{
+	public static String CLASS_NAME = "CreateTradeRulesetup: ";
+	public void sampleMethod()throws Exception{
+		System.out.println(CLASS_NAME + "Begining of the Method...");
+
+		System.out.println(CLASS_NAME + "End of the Method...");
+	}//end of method
+
+	public void customerDetails(String p_strName)throws Exception{
+		System.out.println(CLASS_NAME + "Begining of the Method...");
+
+		System.out.println(CLASS_NAME + "End of the Method...");
+	}//end of method
+
+}//end of class
+
+----------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
